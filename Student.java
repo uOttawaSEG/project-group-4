a2301c5f9d4a954a0f8c91dddbd4588b9fd417a3
Nava Tavasoli <103539975+navatavasoli@users.noreply.github.com>
2025-09-24 17:01:13 -0400

    Added files

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student extends User {
+    private String program;
+
+    public Student(String firstName, String lastName, String email, String phoneNumber, String program) {
+        super(firstName, lastName, email, phoneNumber);
+        this.program = program;
+    }
+
+    // getters and setters
+    public String getProgram() {
+        return program;
+    }
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    @Override
+    public String getRole() {
+        return "Student";
+    }   
+}
---
+++ Tutor.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Tutor extends User  {
+    private String highestDegree;
+    private List<String> coursesOffered;
+
+    public Tutor(String firstName, String lastName, String email, String phoneNumber, String highestDegree, List<String> coursesOffered) {
+        super(firstName, lastName, email, phoneNumber);
+        this.highestDegree = highestDegree;
+        this.coursesOffered = coursesOffered;
+    }
+
+    // getters and setters
+
+    public String getHighestDegree() {
+        return highestDegree;
+    }
+
+    public void setHighestDegree(String highestDegree) {
+        this.highestDegree = highestDegree;
+    }
+
+    public List<String> getCoursesOffered() {
+        return coursesOffered;
+    }
+
+    public void setCoursesOffered(List<String> coursesOffered) {
+        this.coursesOffered = coursesOffered;
+    }
+
+    @Override
+    public String getRole() {
+        return "Tutor";
+    }
+
+}

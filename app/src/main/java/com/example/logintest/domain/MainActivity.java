a53321f1b758bd2eb87bdcda00c919283afd3817
Daniela Bordeianu <danabordeianu55@gmail.com>
2025-11-30 11:41:08 -0500

    Linked the Main (login or register) page to the fancy layout

--- app/src/main/java/com/example/logintest/domain/MainActivity.java
+++ app/src/main/java/com/example/logintest/domain/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.loginorregister);
+        setContentView(R.layout.loginorregister_fancy);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearLayout4), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
